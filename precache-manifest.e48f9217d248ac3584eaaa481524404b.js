c8d5559dff521bc28e4751efb3588f3333b6502b
Poyap <ryougiako@gmail.com>
2021-09-10 21:34:46 +0800

    deploy

---
+++ precache-manifest.e48f9217d248ac3584eaaa481524404b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e37b61cb65e353c27905",
+    "url": "/qinjianlicai-website/css/app.ce0cb8fe.css"
+  },
+  {
+    "revision": "d4edad132aae546f31330b8e307573f9",
+    "url": "/qinjianlicai-website/img/qrcode.d4edad13.png"
+  },
+  {
+    "revision": "cbe86b199c48bf0bcf385be01e294130",
+    "url": "/qinjianlicai-website/index.html"
+  },
+  {
+    "revision": "e37b61cb65e353c27905",
+    "url": "/qinjianlicai-website/js/app.e363595a.js"
+  },
+  {
+    "revision": "7bd2e1f64eebbeda9a11",
+    "url": "/qinjianlicai-website/js/chunk-vendors.463b5725.js"
+  },
+  {
+    "revision": "57cb4d0ce207a5ff1b840bf390516701",
+    "url": "/qinjianlicai-website/manifest.json"
+  },
+  {
+    "revision": "d4edad132aae546f31330b8e307573f9",
+    "url": "/qinjianlicai-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/qinjianlicai-website/robots.txt"
+  }
+]);
